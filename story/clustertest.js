edb85c9ba45dccf6865474fce1bcf222ec6f0560
A Hunter <amanda.hunter@flemingcollege.ca>
2022-02-03 05:34:05 -0500

    organizing icon

---
+++ story/clustertest.js
@@ -0,0 +1,39 @@
+function initMap() void {
+    const map = new google.maps.Map(document.getElementById("map"){
+      zoom: 3,
+      center: { lat: 48, lng: -79 },
+    });
+    // Create an array of alphabetical characters used to label the markers.
+    const iconBase = 
+    
+    
+    
+    
+    
+    
+    
+    
+    const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    // Add some markers to the map.
+    // Note: The code uses the JavaScript Array.prototype.map() method to
+    // create an array of markers based on a given "locations" array.
+    // The map() method here has nothing to do with the Google Maps API.
+    const markers = locations.map((location, i) => {
+      return new google.maps.Marker({
+        position: location,
+        label: labels[i % labels.length],
+      });
+    });
+    // Add a marker clusterer to manage the markers.
+    new MarkerClusterer(map, markers, {
+      imagePath:
+        "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+    });
+  }
+  const locations = [
+    { lat: 49.758123, lng: -92.65349},
+  { lat: 45.442046, lng: -78.820583},
+  { lat: 48.17812017, lng: -90.22694426},
+ 
+  
+  ];
